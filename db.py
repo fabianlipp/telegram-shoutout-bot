5d9ab9226ade42018e2b2459067c27cc2a7c6a37
Fabian Lipp <fabian.lipp@uni-wuerzburg.de>
2019-12-24 01:06:19 +0100

    Initial code commit

---
+++ db.py
@@ -0,0 +1,95 @@
+import time
+
+from sqlalchemy import Column, Integer, String, Float, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker  #, scoped_session
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    chat_id = Column(Integer, primary_key=True)
+    username = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    time_start = Column(Integer)
+    last_msg = Column(String)
+    is_admin = Column(Boolean)
+
+    def __init__(self, chat_id, username, first_name, last_name, time_start=None):
+        self.chat_id = chat_id
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+
+        if time_start is not None:
+            self.time_start = time_start
+        else:
+            self.time_start = int(time.time())
+        self.last_msg = None
+
+    def __repr__(self):
+        return "<User(chat_id='%s', username='%s', first_name='%s', last_name='%s', time_start='%s', last_msg='%s')>" %(self.chat_id, self.username, self.first_name, self.last_name, self.time_start, self.last_msg)
+
+
+class Channel(Base):
+    __tablename__ = "channels"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+
+
+class MyDatabase:
+    db_engine = None
+
+    def __init__(self, filename):
+        self.db_engine = create_engine('sqlite:///' + filename)
+        try:
+            # TODO: Check if schema correct if already existing
+            Base.metadata.create_all(self.db_engine)
+            #print("Tables created")
+        except Exception as e:
+            print("Error occurred during Table creation!")
+            print(e)
+        self.Session = sessionmaker(bind=self.db_engine)
+
+    def get_session(self):
+        return self.Session()
+
+
+class UserDatabase:
+    db = None
+    users = None
+
+    def __init__(self, db):
+        self.db = db
+        session = self.db.get_session()
+        db_users = session.query(User).all()
+        self.users = {user.chat_id: user for user in db_users}
+        session.close()
+
+    def add_user(self, chat_id, username, first_name, last_name):
+        # TODO: Check if user exists
+        user = User(chat_id, username, first_name, last_name)
+        # TODO: Add to list
+        session = self.db.get_session()
+        session.add(user)
+        session.commit()
+        session.close()
+
+    def get_by_chat_id(self, chat_id):
+        return self.users.get(chat_id, None)
+
+
+class ChannelDatabase:
+    db = None
+    channels = None
+
+    def __init__(self, db):
+        self.db = db
+        session = self.db.get_session()
+        db_channels = session.query(Channel).all()
+        self.channels = {channel.id: channel for channel in db_channels}
+        session.close()
---
+++ telegram_shoutout_bot.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python3
+from telegram import Update
+from telegram.ext import Updater, ConversationHandler, CallbackContext
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+
+from telegram_shoutout_bot_conf import BotConf
+import db
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# TODO: Log all queries
+
+CHANNEL, MESSAGE, CONFIRMATION = range(3)
+
+
+class TelegramShoutoutBot:
+    user_database = None  # type: db.UserDatabase
+    channel_database = None  # type: db.ChannelDatabase
+
+    def cmd_start(self, update, context):
+        chat = update.effective_chat
+        self.user_database.add_user(chat.id, chat.username, chat.first_name, chat.last_name)
+        context.bot.send_message(chat_id=chat.id, text="I'm a bot, please talk to me!")
+
+    def cmd_echo(self, update, context):
+        # TODO: DEBUG ONLY
+        context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+    def cmd_admin(self, update, context):
+        user = self.user_database.get_by_chat_id(update.effective_chat.id)
+        if user.is_admin:
+            answer = "Du bist Admin."
+        else:
+            answer = "Du bist derzeit kein Admin. Wende dich mit deiner Chat ID {0} ans Webteam.".format(update.effective_chat.id)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+
+    def cmd_send(self, update: Update, context: CallbackContext):
+        answer = "Kanal eingeben, an den die Nachricht gesendet werden soll.\nVerfügbare Kanäle:"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+        answer = ""
+        for channelId, channel in self.channel_database.channels.items():
+            answer += "{0} - {1}\n".format(channel.name, channel.description)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+        return CHANNEL
+
+    def answer_channel(self, update: Update, context: CallbackContext):
+        # TODO Validate Answer
+        context.user_data["send"] = {}
+        context.user_data["send"]["channel"] = update.message.text
+        answer = "Nachricht eingeben, die gesendet werden soll."
+        context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+        return MESSAGE
+
+    def answer_message(self, update: Update, context: CallbackContext):
+        # TODO Store answer
+        context.user_data["send"]["message"] = update.message
+        # TODO: Möglichkeit mehrere Nachrichten einzugeben
+        answer = "Versand der Nachricht bestätigen oder Abbrechen mit /cancel."
+        context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+        return CONFIRMATION
+
+    def answer_confirmation(self, update: Update, context: CallbackContext):
+        # TODO Nachricht versenden
+        answer = "Diese Daten waren gespeichert:"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+        answer = "Kanalname: {0}".format(context.user_data["send"]["channel"])
+        context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+        # TODO Muss hier die verschiedenen Nachrichten-Typen unterscheiden (Bilder usw.)
+        #  https://github.com/91DarioDev/ForwardsCoverBot
+        message = context.user_data["send"]["message"]
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message.text)
+        return ConversationHandler.END
+
+    def cancel_send(self, update, context):
+        # TODO
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Received cancel")
+        pass
+
+    def error(self, update, context):
+        """Log Errors caused by Updates."""
+        logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+    def __init__(self):
+        my_database = db.MyDatabase(BotConf.database_file)
+        self.user_database = db.UserDatabase(my_database)
+        self.channel_database = db.ChannelDatabase(my_database)
+
+        updater = Updater(token=BotConf.bot_token, use_context=True)
+        dispatcher = updater.dispatcher
+
+        start_handler = CommandHandler('start', self.cmd_start)
+        dispatcher.add_handler(start_handler)
+        admin_handler = CommandHandler('admin', self.cmd_admin)
+        dispatcher.add_handler(admin_handler)
+
+        conv_handler = ConversationHandler(
+            entry_points=[CommandHandler('send', self.cmd_send)],
+            states={
+                CHANNEL: [MessageHandler(Filters.text, self.answer_channel)],
+                MESSAGE: [MessageHandler(Filters.all, self.answer_message)],
+                CONFIRMATION: [MessageHandler(Filters.text, self.answer_confirmation)]
+            },
+            fallbacks=[CommandHandler('cancel', self.cancel_send)]
+        )
+        dispatcher.add_handler(conv_handler)
+
+        #echo_handler = MessageHandler(Filters.text, self.cmd_echo)
+        #dispatcher.add_handler(echo_handler)
+
+        # log all errors
+        dispatcher.add_error_handler(self.error)
+
+        updater.start_polling()
+        updater.idle()
+
+
+if __name__ == '__main__':
+    bot = TelegramShoutoutBot()
+
