32c7614bf426fd361787804d1de6711771333d2a
tmaex <l@tmaex.de>
2020-01-30 23:27:14 +0100

    fix: /subscribe; add: support for URL path; add: style for registration form

--- webinterface/webinterface.py
+++ webinterface/webinterface.py
@@ -18,7 +18,7 @@ def hello_world():
 @app.route('/register/<chat_id>')
 def register(chat_id):
     token = request.args.get('token')
-    return render_template('register_form.html', chat_id=chat_id, token=token)
+    return render_template('register_form.html', url_path=BotConf.url_path, chat_id=chat_id, token=token)
 
 
 @app.route('/register/<chat_id>/login', methods=['POST'])
@@ -28,13 +28,16 @@ def register_login(chat_id):
     password = request.form['password']
     ldap_account = BotConf.ldap_username_template.format(username)
     if not ldap_access.check_credentials(ldap_account, password):
-        return render_template('register_login_fail.html', reason="ldap")
+        return render_template('register_login_fail.html', reason="ldap", url_path=BotConf.url_path,
+                               chat_id=chat_id, token=token)
     with db.my_session_scope(my_database) as session:  # type: db.MyDatabaseSession
         user = session.get_user_by_chat_id(chat_id)
         if user is None:
-            return render_template('register_login_fail.html', reason="chat_id")
+            return render_template('register_login_fail.html', reason="chat_id", url_path=BotConf.url_path,
+                                   chat_id=chat_id, token=token)
         if user.ldap_register_token != token:
-            return render_template('register_login_fail.html', reason="token")
+            return render_template('register_login_fail.html', reason="token", url_path=BotConf.url_path,
+                                   chat_id=chat_id, token=token)
         user.ldap_account = ldap_account
         user.ldap_register_token = None
         session.commit()
