5614ec3457eb83e766d1c1b43cccec96d660e93b
tmaex <l@tmaex.de>
2020-02-22 00:45:45 +0100

    add: using Flask.g for global vars

--- webinterface/webinterface.py
+++ webinterface/webinterface.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, g, request, render_template
 import db
 import bot_ldap
 from telegram_shoutout_bot_conf import BotConf
@@ -9,6 +9,11 @@ my_database = db.MyDatabase(BotConf.database_file)
 ldap_access = bot_ldap.LdapAccess(BotConf.ldap_server, BotConf.ldap_user,
                                   BotConf.ldap_password, BotConf.ldap_base_group_filter)
 
+@app.before_request
+def before_request():
+    g.url_libs = BotConf.url_libs
+    g.url_path = BotConf.url_path
+
 
 @app.route('/')
 def hello_world():
@@ -18,7 +23,7 @@ def hello_world():
 @app.route('/register/<chat_id>')
 def register(chat_id):
     token = request.args.get('token')
-    return render_template('register_form.html', url_path=BotConf.url_path, chat_id=chat_id, token=token)
+    return render_template('register_form.html', chat_id=chat_id, token=token)
 
 
 @app.route('/register/<chat_id>/login', methods=['POST'])
@@ -28,15 +33,14 @@ def register_login(chat_id):
     password = request.form['password']
     ldap_account = BotConf.ldap_username_template.format(username)
     if not ldap_access.check_credentials(ldap_account, password):
-        return render_template('register_login_fail.html', reason="ldap", url_path=BotConf.url_path,
-                               chat_id=chat_id, token=token)
+        return render_template('register_login_fail.html', reason="ldap", chat_id=chat_id, token=token)
     with db.my_session_scope(my_database) as session:  # type: db.MyDatabaseSession
         user = session.get_user_by_chat_id(chat_id)
         if user is None:
-            return render_template('register_login_fail.html', reason="chat_id", url_path=BotConf.url_path,
+            return render_template('register_login_fail.html', reason="chat_id",
                                    chat_id=chat_id, token=token)
         if user.ldap_register_token != token:
-            return render_template('register_login_fail.html', reason="token", url_path=BotConf.url_path,
+            return render_template('register_login_fail.html', reason="token",
                                    chat_id=chat_id, token=token)
         user.ldap_account = ldap_account
         user.ldap_register_token = None
